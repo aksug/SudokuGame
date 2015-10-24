8dfd69c333e8db98c82b648e5ad377023371bf44
unknown <C5839@EBKNTBOOK-0469.ebankit.local>
2015-10-24 13:29:35 +0100

    bootn sheet

--- src/main/java/sudoku/example/com/sudoku/MenuLevels.java
+++ src/main/java/sudoku/example/com/sudoku/MenuLevels.java
@@ -1,6 +1,7 @@
 package sudoku.example.com.sudoku;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
@@ -8,6 +9,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.cocosw.bottomsheet.BottomSheet;
+import com.software.shell.fab.ActionButton;
+
 
 public class MenuLevels extends AppCompatActivity {
     private String TAG = "Menu_levels";
@@ -20,6 +25,27 @@ public class MenuLevels extends AppCompatActivity {
         //to allow Up
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Log.d(TAG, "in onCreate()");
+
+        final MenuLevels action = this;
+        ActionButton fab = (ActionButton) findViewById(R.id.action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new BottomSheet.Builder(action, R.style.BottomSheet_Dialog)
+                        .title("New")
+                        .grid() // <-- important part
+                        .sheet(R.menu.menu_bottom_sheet)
+                        .listener(new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO
+                            }
+                        }).show();
+
+//                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
