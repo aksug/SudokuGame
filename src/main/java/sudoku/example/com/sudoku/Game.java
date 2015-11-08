51c695be3a33c7b09078511f0406781771e6a74b
unknown <a-corelli@wp.pl>
2015-11-08 21:21:04 +0000

    check if user name deleted

--- src/main/java/sudoku/example/com/sudoku/Game.java
+++ src/main/java/sudoku/example/com/sudoku/Game.java
@@ -12,6 +12,8 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupWindow;
